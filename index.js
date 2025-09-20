beccbcfe17d39712094259e063a2c47422323b00
su ureyil <suureyil@192.168.1.52>
2025-09-20 20:01:03 +0300

    Bcrypt -> bcryptjs ve env bağlantısı güncellendi

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ const User = require('./models/User');
 const Post = require('./models/Post');
 const Comment = require('./models/Comment');
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Atlas bağlantısı kuruldu.'))
-  .catch(err => console.error(err));
+// Connect to MongoDB Atlas using environment variable
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Atlas bağlantısı kuruldu.'))
+.catch(err => console.error('MongoDB bağlantı hatası:', err));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
